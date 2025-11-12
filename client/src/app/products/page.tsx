965de663b95357dac3d9ab9fcb4739acea61d4c5
Ritika Labana <107995702+ritikalabana@users.noreply.github.com>
2025-11-12 11:37:33 +0530

    Fetch products by category from API
    Fetch products based on category from API and log results.

--- client/src/app/products/page.tsx
+++ client/src/app/products/page.tsx
@@ -6,6 +6,18 @@ const ProductsPage = async ({
   searchParams: Promise<{ category: string }>;
 }) => {
   const category = (await searchParams).category;
+
+  const res = await fetch(
+    category 
+    ? `https://dummyjson.com/products/category/${category}`
+    : "https://dummyjson.com/products"
+  );
+
+    const data = await res.json();
+    const products = data.products || [];
+
+    console.log("Fetched products:",products);
+  
   return (
     <div className="">
       <ProductList category={category} params="products"/>
